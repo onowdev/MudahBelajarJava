a545e7e1a1cadc85fba9b4caaf4266bcd6b923c1
onowdev <onowdev@gmail.com>
2023-05-27 22:04:03 +0700

    method > 1

---
+++ src/com/onowdev/newcpter/Method04.java
@@ -0,0 +1,10 @@
+package com.onowdev.newcpter;
+
+public class Method04 {
+    public static void main(String[] args) {
+        jumlah(900,800);
+    }
+    public static void jumlah(int a, int b){
+        System.out.println("Jumlah = " + (a+b) );
+    }
+}
