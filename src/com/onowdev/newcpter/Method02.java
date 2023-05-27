84de88cd505f56b18c3710b6bbd480ecf3788ce6
onowdev <onowdev@gmail.com>
2023-05-27 22:00:56 +0700

    method Sederhana dengan Argument

---
+++ src/com/onowdev/newcpter/Method02.java
@@ -0,0 +1,10 @@
+package com.onowdev.newcpter;
+
+public class Method02 {
+    public static void main(String[] args) {
+        salam2("Agus Sugiono");
+    }
+    public static void salam2(String nama){
+        System.out.printf("Assalamualaikum " + nama);
+    }
+}
