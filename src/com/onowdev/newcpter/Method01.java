ff7ab7b7b91ccf8f7e238bba5950250537dff35b
onowdev <onowdev@gmail.com>
2023-05-27 22:00:43 +0700

    method Sederhana

---
+++ src/com/onowdev/newcpter/Method01.java
@@ -0,0 +1,12 @@
+package com.onowdev.newcpter;
+
+public class Method01 {
+    public static void main(String[] args) {
+        salam();
+    }
+
+    public static void salam() {
+        System.out.println("Assalamualaikum");
+
+    }
+}
